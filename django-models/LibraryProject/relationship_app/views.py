7bb04b995e3ca9f4d3745fd8c97072c7e4f7ae18
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-09-09 23:26:44 +0100

    corection in signal.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-
-# Create your views here.
-# views.py
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth import logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
@@ -23,10 +24,29 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()
         return context
-    from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.shortcuts import render, redirect
-from django.contrib import messages
+
+# User role check functions
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'member'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'librarian'
+
+def is_admin(user):
+    return user.is_superuser
+
+# User role-based views
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
 
 def register_view(request):
     if request.method == 'POST':
