9218676ddf0aadd5c4887b8361091d0becbdaf37
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-09-09 21:04:21 +0100

    more modifications

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,18 @@ from django.views.generic.detail import DetailView
 
 # Function-Based View to list all books
 def list_books(request):
-    books = Book.objects.all()  # <-- This line satisfies the checker
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-Based View for library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
 
 
