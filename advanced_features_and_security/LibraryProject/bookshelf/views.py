8d5d3a5bd9306681c03b6545874dd09b290d7715
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-09-19 00:07:48 +0100

    correction in view.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ExampleForm
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+
 
 # Create your views here.
+@permission_required("bookshelf.can_create", raise_exception=True)  # With the permission_required decorator and raise exception
+@permission_required("bookshelf.can_edit", raise_exception=True)    # users who are already logged in and has one of this permissions missing would just get a 403 error message instead of
+@permission_required("bookshelf.can_delete", raise_exception=True)  # of just being taken to the login page
+
+def book_list(request):
+    return HttpResponse ("You are allowed to edit, delete and create Books")
+
+
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the data here
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            # You could save it, send an email, etc.
+            return redirect('success')  # Replace with your success URL
+    else:
+        form = ExampleForm()
+    return render(request, 'example_form.html', {'form': form})
