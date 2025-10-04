50d74ec4724ae31cf214fcabc6b66b907824f1fd
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-10-04 11:01:14 +0100

    added url

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+
+# List all books (GET) - anyone can view
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # public read-only access
+
+# Retrieve book details (GET by ID) - anyone can view
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Create a new book (POST) - only authenticated users
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Update existing book (PUT/PATCH) - only authenticated users
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Delete a book (DELETE) - only authenticated users
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
