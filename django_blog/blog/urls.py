a0062051406f225abfd8533458be2ba30eda1b09
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-10-07 00:47:33 +0100

    more corrections

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -34,5 +34,7 @@ urlpatterns += [
     path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+
 ]
 
