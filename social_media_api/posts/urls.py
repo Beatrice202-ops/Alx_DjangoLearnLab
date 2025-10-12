671bf259a5681337b28730771fcfc8cd939fd359
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-10-12 11:56:12 +0100

    addition of path in post url

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, FeedAPIView
+from .views import PostViewSet, CommentViewSet, FeedAPIView, LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -9,4 +9,10 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedAPIView.as_view(), name='feed'),
+]
+
+urlpatterns = [
+    path('feed/', FeedAPIView.as_view(), name='feed'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
