3cfc3420bf7ee1eabb69e92a4566c2130db6b3b5
Beatrice202-ops <uzomabeatrice3@gmail.com>
2025-10-12 16:08:44 +0100

    correction in account serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -42,3 +42,26 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email']
+        
+        from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        # ✅ Use create_user() here
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
+
+
